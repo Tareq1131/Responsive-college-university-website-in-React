c52299307a936d97bf0acd27457624b3a696dca0
Tareq1131 <tareqislam076@gmail.com>
2024-02-26 18:29:33 +0600

    project setup and create navbar

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Navbar from "./components/Navbar/Navbar"
+
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+    </div>
+  )
+}
+
+export default App
+
+
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,21 @@
+// import React from 'react'
+import "./Navbar.css";
+import logo from "../../assets/logo.png";
+
+function Navbar() {
+  return (
+    <nav className="container">
+      <img src={logo} alt="" className="logo"/>
+      <ul>
+        <li>Home</li>
+        <li>Program</li>
+        <li>About us</li>
+        <li>Campus</li>
+        <li>Testimonials</li>
+        <li><button className="btn">Contact us</button></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
